2121024c36bc5ff2b41d64d95fad91a11eb4c79c
sal442003-HVLAB <sal442003@gmail.com>
2026-08-26 17:40:08 +0330

    Install EULUMDAT polar orientation convention

--- grid_results_runtime.py
+++ grid_results_runtime.py
@@ -14,6 +14,7 @@ from PySide6.QtWidgets import QFileDialog, QMessageBox, QSizePolicy
 from grid_results_visual_refinements import install_grid_results_visual_refinements
 from eulumdat_results_refinements import install_eulumdat_results_refinements
 from eulumdat_dual_plane_refinements import install_eulumdat_dual_plane_refinements
+from eulumdat_polar_orientation import install_eulumdat_polar_orientation
 from measurement_run import measurement_data_directory
 from results_charts import CALCULATED_LUX
 from results_grid_charts import GridResultsCharts, extract_grid_data
@@ -42,6 +43,7 @@ def attach_grid_results_runtime(window):
     install_grid_results_visual_refinements()
     install_eulumdat_results_refinements()
     install_eulumdat_dual_plane_refinements()
+    install_eulumdat_polar_orientation()
 
     workspace = getattr(window, "results_workspace_controller", None)
     if workspace is None:
