14917aa17a8a75f404f1bc350246a361693b3f71
sal442003-HVLAB <sal442003@gmail.com>
2026-09-13 09:28:19 +0330

    Enable continuous P-9710 CW and peak readings

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from motor_control_refinement import attach_motor_control_refinement
 from luxmeter_controls import attach_luxmeter_controls
 from effective_intensity_test import attach_effective_intensity_test
 from p9710_mode_workspace import attach_p9710_mode_workspace
+from p9710_continuous_runtime import attach_p9710_continuous_runtime
 from luxmeter_workspace_tabs import attach_luxmeter_workspace_tabs
 from tabbed_layout import organize_main_window_tabs
 from execution_mode_controls import attach_execution_mode_controls
@@ -198,6 +199,7 @@ def main():
     organize_main_window_tabs(window)
     attach_effective_intensity_test(window)
     attach_p9710_mode_workspace(window)
+    attach_p9710_continuous_runtime(window)
     attach_luxmeter_workspace_tabs(window)
 
     attach_results_workspace(window)
